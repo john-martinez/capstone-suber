2b52acfbce85fdf886813543f1b5aaee93b9f19e
johnray-martinez <jrvmartinez@icloud.com>
2020-03-27 19:11:08 -0400

    reverted shake changes

--- src/components/GameArea/GameArea.jsx
+++ src/components/GameArea/GameArea.jsx
@@ -13,7 +13,7 @@ import bgsound from '../../assets/sounds/main_menu.mp3';
 import deadbg from '../../assets/sounds/dead.mp3';
 import thunder from '../../assets/sounds/thunder.mp3';
 import crash from '../../assets/sounds/crash.mp3';
-import Shake from 'shake.js';
+
 export default class GameArea extends Component {
     state = { 
         crashed: false, 
@@ -62,17 +62,6 @@ export default class GameArea extends Component {
         return speech;
     }
     componentDidMount(){ 
-        let shakeEvent = new Shake({
-            threshold: 15,
-            timeout: 1000
-        })
-
-        shakeEvent.start();
-
-        window.addEventListener('shake', ()=>{
-            this.setState({clicked: true})
-        }, false)
-
         document.addEventListener('click', ()=>{
             !this.state.clicked ? this.setState({clicked: true}) : this.a = '';
         })
