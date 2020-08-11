54cf68d31ea7eafce88de435ef60dcfddfbe64f4
johnray-martinez <jrvmartinez@icloud.com>
2020-08-11 17:53:19 -0400

    created scoresheet

--- src/components/HighScores/HighScores.jsx
+++ src/components/HighScores/HighScores.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './HighScores.scss';
 export default class HighScores extends Component {
   state = {
     scores: [],
@@ -14,12 +15,23 @@ export default class HighScores extends Component {
 
   render(){
     const { scores } = this.state;
-    console.log(scores);
 
     if (scores.length){
       return (
         <div className="highscores">
-          
+          <div className="highscores__item">
+            <div></div>
+            <div>Player</div>
+            <div>Score</div>
+          </div>
+
+          { scores.map((score, i) => (
+            <div className="highscores__item">
+              <div>{ `${i + 1}. ` } </div>
+              <div>{ score.playerName }</div>
+              <div>{ score.playerScore }</div>
+            </div>
+          )) }
         </div>
       );
     } else {
